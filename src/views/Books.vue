695fd34ccc9a1dfb80220fc0204c0a343e072aab
Ma Yanbin <myanbin@gmail.com>
2020-01-08 21:44:44 +0800

    完成项目脚手架

---
+++ src/views/Books.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="books">
+    <h1>图书管理器</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'books',
+  data: () => {
+    return {};
+  }
+}
+</script>
+
+<style>
+
+</style>
